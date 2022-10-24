afddc9cddbdeed8d093eac7149c54d1231deaa7e
natacha <nat_bartu@hotmail.com>
2022-10-24 14:34:37 +1000

    add values into input type, and create a function for adding the todos

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 const App = () => {
-  // uuidv4() - call dependency
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        id: uuidv4,
+      },
+    ]);
+  };
 
   return (
     <div className="App">
       <div className="container">
-        <form>
-          <input type="text" placeholder="Add a task..." value={inputValue} />
+        <form onSubmit={addTodo}>
+          <input
+            type="text"
+            placeholder="Add a task..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
           <button type="submit">Add</button>
         </form>
       </div>
