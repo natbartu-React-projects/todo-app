c399781d0e98b2565cb77102eff4cf305378a462
natacha <nat_bartu@hotmail.com>
2022-10-24 14:25:10 +1000

    add form structure into the app file, create a components  folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 const App = () => {
   // uuidv4() - call dependency
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <form>
+          <input type="text" placeholder="Add a task..." value={inputValue} />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
