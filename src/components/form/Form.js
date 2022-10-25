81c73bf9d6cc998e631e9e1013338cffcf502826
natacha <nat_bartu@hotmail.com>
2022-10-25 15:06:11 +1000

    add local storage and useEffect, useRef  hooks functionality

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./form.css";
 
 const Form = () => {
+  const firstRender = useRef(true);
+
   const [inputValue, setInputValue] = useState("");
 
   const [todos, setTodos] = useState([]);
@@ -22,11 +24,31 @@ const Form = () => {
     setInputValue("");
   };
 
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+    } else {
+      localStorage.setItem("Todos", JSON.stringify([...todos]));
+    }
+  }, [todos]);
+
+  useEffect(() => {
+    if (localStorage.getItem("Todo") !== null) {
+      const newTodos = localStorage.getItem("Todo");
+      setTodos(JSON.parse([...todos, newTodos]));
+    }
+  }, []);
+
   return (
     <div>
       {" "}
       <form onSubmit={addTodo}>
         <input
+          autoFocus
           type="text"
           placeholder="Add a task..."
           value={inputValue}
@@ -37,7 +59,10 @@ const Form = () => {
       {todos.map((todo) => (
         <div key={todo.id} className="todo">
           <p>{todo.text}</p>
-          <i className="far fa-trash-alt"></i>
+          <i
+            onClick={() => removeTodo(todo.id)}
+            className="far fa-trash-alt"
+          ></i>
         </div>
       ))}
     </div>
